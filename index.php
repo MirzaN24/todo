49c9f1c3a39dd0cfe564168e0e016b7ab439a690
MirzaN24 <mirza.novalic@stu.ibu.edu.ba>
2022-07-29 17:44:13 +0200

    database connect succesfully //result 1

--- index.php
+++ index.php
@@ -2,4 +2,24 @@
 
 echo "Idemooo web";
 
+$servername = "localhost";
+$username = "todo";
+$password = "todo";
+$schema = "todo";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+
+  $stmt = $conn->prepare("SELECT * FROM todos");
+  $result = $stmt->execute();
+  print_r($result);
+
+
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
  ?>
